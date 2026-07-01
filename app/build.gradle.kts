50877578b78e3fd59b4d23c47b20eb92075a53ed
max rovkin <rovkinmax@gmail.com>
2026-07-01 10:11:38 +0500

    Release 1.1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
     alias(libs.plugins.androidx.baselineprofile)
 }
 
-val currentVersion = "1.0.0"
+val currentVersion = "1.1.0"
 
 /**
  * Reads CLIENT_SECRET from local.properties or system environment variable
@@ -72,7 +72,7 @@ android {
         buildConfigField("String", "TMDB_READ_ACCESS_TOKEN", "\"${getTmdbReadAccessToken()}\"")
 
         ndk {
-            abiFilters += "arm64-v8a"
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
         }
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
